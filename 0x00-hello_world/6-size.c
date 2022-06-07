d1218a8e8cfd183627a8e9a7146783dbf3ae1ff0
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-07 15:25:58 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size is not grandeur, and territory does not make a nation");
+	return (0);
+}
