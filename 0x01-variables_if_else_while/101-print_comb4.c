9ff275fc9f02124a68200442061b9abe3ad5d152
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 10:04:32 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m, l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n <= 7; n++)
+	{
+		for (m = 0; m <= 8; m++)
+		{
+		for (l = 0; l < 10; l++)
+		{
+		putchar((n % 10) + '0');
+	        putchar((m % 10) + '0');
+	        putchar((l % 10) + '0');
+	        if (n == 7 && m == 8 && l == 9)
+		continue;
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
