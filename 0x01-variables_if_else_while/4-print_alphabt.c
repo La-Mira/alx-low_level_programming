a448d2cbe9cea3248ad04dff9cc9212a9bb428db
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 05:54:51 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabets except q and e in lowercase,
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 'a'; n <= 'z'; n++, n != 'q' && n != 'e')
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
