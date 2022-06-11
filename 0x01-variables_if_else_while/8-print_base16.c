c1bb5b2e30c6c7e68f6eb7463e7046282062fbf1
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 08:44:05 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line
+ *
+ * Returns: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n < 16; n++)
+		putchar(n % 16 + '0');
+	putchar('\n');
+	return (0);
+}
