f766209df8ba4c2fe790adc5d7165b63289049d3
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 09:46:32 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	for (n = 0; n < 9; n++)
 	{
-		for (m = n + 1; m <= 9; m++)
+		for (m = n + 1; m < 10; m++)
 		{
 			putchar((n % 10) + '0');
 			putchar((m % 10) + '0');
-		}
 		if ((n == 8) && (m == 9))
 			continue;
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
