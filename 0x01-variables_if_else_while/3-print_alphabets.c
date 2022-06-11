73201793f8abeb2af7193ae3b85bdaab11363bdd
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 05:38:28 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main -  prints the alphabet in lowercase, and 
+ * main -  prints the alphabet in lowercase, and
  * then in uppercase, followed by a new line
  *
  * Return: Always 0 (Success)
