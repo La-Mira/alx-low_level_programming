4bcd4518e7735ff7206eb45b277f42dd7175fad1
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 07:34:52 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -  prints all single digit numbers of base 10 
+ * starting from 0, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
