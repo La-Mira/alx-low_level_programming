152f97b8d679509f79139755ae22c18118104175
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 15:27:25 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,6 +4,7 @@
 
 /**
  * main - prints all possible combinations of two two-digit numbers
+ * ranging from 0-99, separated by a comma followed by a space
  *
  * Return: Always 0 (Success)
  */
@@ -13,19 +14,22 @@ int main(void)
 	int i, j;
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);								putchar(' ');									putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)								putchar(',');
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i == 98 && j == 99)
+					continue;
+					putchar(',');
+					putchar(' ');
 			}
 		}
 	}
 				putchar('\n');
 				return (0);
-
 }
