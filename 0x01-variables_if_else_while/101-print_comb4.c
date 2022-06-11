f653fc2aff00ce0ae9e0e1553f4ce1f296b67e1c
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 12:36:26 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,9 +3,7 @@
 #include <time.h>
 
 /**
- * main - prints all possible different combinations of three digits,
- * putting into consideration the same combinations
- * with the smallest combinations of the combinations printed
+ * main - prints all possible different combinations of three digits
  *
  * Return: Always 0 (Success)
  */
@@ -16,26 +14,25 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	for (n = 0; n <= 7; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = n + 1; m <= 8; m++)
+		for (m = 49; m < 58; m++)
 		{
-			for (l = m + 1; l < 10; l++)
-			{
-				if (n == 7 && m == 8 && l == 9)
-				putchar((n % 10) + '0');
-				putchar((m % 10) + '0');
-				putchar((l % 10) + '0');
-				continue;
-
-				if (n < m < l)
+			for (l = 50; l < 58; l++)							{
+				if (l > m && m > n)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
-	}
+							}
 	putchar('\n');
 	return (0);
 }
