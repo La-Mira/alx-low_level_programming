bd1424818aa2cc9763dc69ec692c5b2a5ced6306
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-11 09:38:44 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n,m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n < 9; n++)
+	{
+		putchar((n % 10) + '0');
+		for (m = n + 1; m <= 9; m++)
+		{
+			putchar((m % 10) + '0');
+		}
+		if ((n == 8) && (m == 9))
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
