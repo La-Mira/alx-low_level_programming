03b9b727dd3e317760ae57735d005094e0ce6c52
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-12 02:16:29 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,11 +18,11 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((i / 10) + 00);
+				putchar((i % 10) + 00);
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar((j / 10) + 00);
+				putchar((j % 10) + 00);
 
 				if (i != 98 || j != 99)
 				{
