f2cc16a1bd873176b5f503b049f7a9c021ba7972
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-12 09:17:43 -0700

    _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - prints _putchar, followed by a new line
+ * main -  prints _putchar, followed by a new line
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	putchar(``);
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
