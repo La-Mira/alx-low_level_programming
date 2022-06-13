bbdb0e87969e2c3fced03b0f1d18852ff28c058e
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-13 15:32:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char n;
+	n = 'a';
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+		_putchar('\n');
+		print_alphabet();
+	        return (0);
+	}
+}
+
+
