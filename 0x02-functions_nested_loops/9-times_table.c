07ff061c86db11b15574958538f52a229168b04b
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-15 11:27:39 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,38 +3,36 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  *
- * Return: 0
  */
 
 void times_table(void)
 {
-	int i, j, n;
+	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			n = i * j;
+			k = j * i;
 			if (j == 0)
 			{
-				_putchar(n + '0');
+				_putchar(k + '0');
 			}
-			if (n <= 9 && j != 0)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(n + '0');
+				_putchar(k + '0');
 			}
-		else
+			else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
 	}
 }
-
