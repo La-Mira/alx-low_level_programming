4d09e43f64c05b94be3a97ec2f32fce446703fe3
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-15 03:46:07 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,15 @@
  * Return: Always 0
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-	char n;
 	int i;
+	char n;
 
-	for (i = 0; i < 10; 1++)
+	while (i++ < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
-		{
 			_putchar(n);
-		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }	
