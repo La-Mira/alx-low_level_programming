b904f28cd62fcc45713926d43f7876815b4b0b14
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-15 04:14:48 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
  * _islower - checks for lowercase character
+ * @c: the character to check
  *
  * Return: 1 if character is lowercase, 0 otherwise
  */
 
 int _islower(int c);
 {
-	if (c = 'a'; c <= 'z'; c++)
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
