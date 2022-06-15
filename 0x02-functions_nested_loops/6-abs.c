f3a16e4f9f9267a56a72613e8dc954746ad05904
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-15 05:47:19 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: the integer to be computed
+ *
+ * Return: the absolute value of int
+ */
+
+int _abs(int);
+{
+	if (int >= 0)
+		return (n);
+	else
+		return (-n);
+}
