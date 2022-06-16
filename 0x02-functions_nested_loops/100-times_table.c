d2b054081b31d481fe6ae12cfebf146ea96407c3
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-16 11:33:12 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: the number of the times table to be printed
- *
- * Return: 0
+ * @n: number of the times table
  */
 
 void print_times_table(int n)
@@ -17,49 +14,33 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
-			{
 				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k == 2)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k <= 15)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10)) % 10 + '0');
-					_putchar((k % 10) + '0');
-				}
-				_putchar('\n');
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			} else if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10 && k < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			} else if (k >= 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
