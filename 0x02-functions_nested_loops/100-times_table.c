17af226b705887e6cdde874f35fc7eedf1c12955
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-15 23:01:40 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,6 +22,12 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
+				} else if (k == 2)
+				{
+					_putchar(k + '2');	
+				} else if (k >= 10 && k <= 15)
+				{
+					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
