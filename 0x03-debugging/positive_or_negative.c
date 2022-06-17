19451b5dd3866d467d35a5b3757dd6f6d7cda03d
Aminat Erinfolami <erinfolamiaminat027@yahoo.com>
2022-06-17 03:43:54 -0700

    positive_or_negative.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * postitive_or_negative - prints
+ * if integer is positive or negative
+ * @i: integer to be printed if it is positive or negative
+ */
+
+void positive_or_negative(int i)
+{
+	i = 98;
+	printf("%d", i);
+}
